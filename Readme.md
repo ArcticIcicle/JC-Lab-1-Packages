45cb02a965fc96b1ef3df5ca156021ae4ed07643
ArcticIcicle <cjnchoa@gmail.com>
2025-07-31 13:55:44 +0800

    Lab: Java Packages Submission

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Lab Java Packages
+
+Submitted by: Christian Choa <br/>
+Date: July 31, 2025
+
+### Adding:
+![Screenshot of output](output.png)
+
+
+
+
---
+++ src/main/java/org/example/main/Main.java
@@ -0,0 +1,22 @@
+package org.example.main;
+import org.example.packageone.ClassOne;
+import org.example.packagetwo.ClassTwo;
+import org.example.packagethree.ClassThree;
+import static java.lang.System.out;
+
+// Import here manually
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        ClassOne classone = new ClassOne();
+        out.println(classone.greet());
+
+        ClassTwo classtwo = new ClassTwo();
+        out.println(classtwo.greet());
+
+        ClassThree classthree = new ClassThree();
+        out.println(classthree.greet());
+
+    }
+}
